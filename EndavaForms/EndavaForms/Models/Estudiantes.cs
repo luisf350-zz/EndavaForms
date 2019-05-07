359d95d8c224b61350ac21e87d734f6199da335b
luisf350@gmail.com <luisf350@hotmail.com>
2019-05-07 10:47:33 -0500

    Se agrega el nuget de Realm y se adiciona la pantalla de LocalDb y su ViewModel

---
+++ EndavaForms/EndavaForms/Models/Estudiantes.cs
@@ -0,0 +1,19 @@
+﻿using Realms;
+using System;
+
+namespace EndavaForms.Models
+{
+    public class Estudiantes : RealmObject
+    {
+        [PrimaryKey]
+        public string Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public DateTimeOffset FechaNacimiento { get; set; }
+
+        public string NombreCompleto => $"{Nombre} {Apellido}";
+    }
+}
--- EndavaForms/EndavaForms/ViewModels/LocalDbViewModel.cs
+++ EndavaForms/EndavaForms/ViewModels/LocalDbViewModel.cs
@@ -1,15 +1,74 @@
-﻿using Prism.Navigation;
+﻿using EndavaForms.Models;
+using Prism.Commands;
+using Prism.Navigation;
 using Prism.Services;
+using Realms;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace EndavaForms.ViewModels
 {
     public class LocalDbViewModel : ViewModelBase
     {
+        #region Atributos
+
+        private ObservableCollection<Estudiantes> _registros;
+
+        #endregion
+
+        #region Propiedades
+
+        public ObservableCollection<Estudiantes> Registros
+        {
+            get => _registros;
+            set => SetProperty(ref _registros, value);
+        }
+
+        #endregion
+
+        #region Commands
+
+        public DelegateCommand NuevoRegistroCommand { get; set; }
+
+        #endregion
+
         #region Constructor
 
         public LocalDbViewModel(INavigationService navigationService, IPageDialogService dialogService) : base(navigationService, dialogService)
         {
+            InstanciarObjetos();
+        }
+
+        #endregion
+
+        #region Metodos Privados
+
+        private void InstanciarObjetos()
+        {
             Title = "Local DB";
+
+            NuevoRegistroCommand = new DelegateCommand(NuevoRegistro);
+
+            CargarRegistros();
+        }
+
+        private void CargarRegistros()
+        {
+            Registros = new ObservableCollection<Estudiantes>();
+
+            Realm realm = Realm.GetInstance();
+
+            IEnumerable<Estudiantes> listaEstudiantes = realm.All<Estudiantes>();
+
+            foreach (Estudiantes lo in listaEstudiantes)
+            {
+                Registros.Add(lo);
+            }
+        }
+
+        private async void NuevoRegistro()
+        {
+            await DialogService.DisplayAlertAsync("Nuevo Registro", "No se tiene implementación", "Ok");
         }
 
         #endregion
